9977c87769c17991203a9641b037fc9d2c281357
Natanot <69844485+Natrexq@users.noreply.github.com>
2021-11-22 16:06:54 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+var Code = Math.floor(Math.random() * 782989);
+
+
+
+
+function Generate(){
+
+    
+
+    var Gene = document.getElementById('getid');
+
+    document.getElementById('genid').innerHTML = Gene;
+
+
+
+
+}
+function Check(){
+
+
+
+}
